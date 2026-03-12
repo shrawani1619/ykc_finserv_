ef130eb13250ce8eae087aaf04e3669e653eb4bc
shrawani1619 <kaleshrawani16@gmail.com>
2026-03-12 15:59:57 +0530

    cors issue fix

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ const app = express();
 app.use(cors({
   origin: [
     "https://ykcfinserv.com",
-    "https://www.ykcfinserv.com"
+    "https://www.ykcfinserv.com",
+    "https://ykc-finserv.vercel.app"
   ],
   credentials: true,
   methods: ["GET","POST","PUT","DELETE","PATCH","OPTIONS"],
